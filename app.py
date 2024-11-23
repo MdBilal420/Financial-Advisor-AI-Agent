32217c1314a0d67590a565aa0b6e184150656efd
Mohammad Bilal <mohdbilal16@gmail.com>
2024-11-23 10:57:15 +0530

    html parser added

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ from lyzr_agent_api.models.environment import EnvironmentConfig, FeatureConfig
 from lyzr_agent_api.models.agents import AgentConfig
 from lyzr_agent_api.models.chat import ChatRequest
 
+from lyzr_automata import Agent,Task
+from lyzr_automata.pipelines.linear_sync_pipeline import LinearSyncPipeline
+from lyzr_automata.ai_models.openai import OpenAIModel
+
+import requests
+from bs4 import BeautifulSoup
+import re
+
 
 from dotenv import load_dotenv,find_dotenv
 
@@ -46,6 +54,7 @@ st.set_page_config(
     page_title = "Investment Advisor Agent"
 )
 
+
 if not openai_api_key:
     openai_api_key = st.sidebar.text_input("Enter your OpenAI API key:", type="password")
 
@@ -53,6 +62,27 @@ os.environ["OPENAI_MODEL_NAME"] = 'gpt-3.5-turbo'
 
 st.title("Investment Advisor Agent")
 st.write("Personalized Investment Portfolio Advisor using Lyzr's API.")
+open_ai_text_completion_model = OpenAIModel(
+    api_key=openai_api_key,
+    parameters={
+        "model":"gpt-4-turbo-preview",
+        "temperature":0.2,
+        "max_tokens":1500
+    }
+)
+
+def get_investment_amount(income, expenses, savings, debt):
+    try:
+        available_for_investment = income - expenses- savings - debt
+
+        # Ensure the investment amount is non-negative
+        investment_amount = max(0, available_for_investment)
+
+        return investment_amount
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
 
 
 def getAdvice():
@@ -63,52 +93,117 @@ def getAdvice():
     agent_description="This agent provides expert financial guidance, offering tailored strategies for wealth management, retirement planning, and investment growth.",
     )
     agent = client.create_agent_endpoint(json_body=agent_config)
-    print("AGENT",agent)
+    
     response = client.chat_with_agent(
         json_body=ChatRequest(
             user_id="user-id",
             agent_id=agent["agent_id"],
-            message="What are the best investment strategies for balancing short-term liquidity needs with long-term wealth growth?",
+            message="What are the best investment amount for balancing short-term liquidity needs with long-term wealth growth?",
             session_id="session-id",
         )
     )
     return response
 
-# print("RESPONSE",response)
+def parseURL():
+    url = "https://www.screener.in/screens/515361/largecaptop-100midcap101-250smallcap251/"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.content, features="html.parser")
+
+        textContent = re.sub(r'\s{4,}','   ',soup.get_text())
+        return textContent
+    except requests.exceptions.RequestException as e:
+        print(f"Error URL not working {url}:{e}")
+        return None
+    
+def extractData(text):
+    try:
+        analyst_agent = Agent(
+            role="analyst",
+            prompt_persona=f"""you are an expert analyst and you have to find out the list of companies from {text} along with their some financial details in tabular format."""
+        )
+
+        task1 = Task(
+            name="stock analysis",
+            model=open_ai_text_completion_model,
+            agent=analyst_agent,
+            instructions=f'Analyse the list of companies from {text} along with their some financial details in tabular format'
+        )
+
+        output = LinearSyncPipeline(
+            name="Analyst Pipeline",
+            completion_message="List fetched",
+            tasks=[
+                task1
+            ]
+        ).run()
+        return output[0]['task_output']
+    except requests.exceptions.RequestException as e:
+        print(f"Error URL not working {url}:{e}")
+        return None
 
 result=[]
-with st.form("my_form"):
+list = []
+investment = ''
+with st.form("personal_finance"):
     col1, col2, col3, col4 = st.columns(4)
-
+    income = 0
+    expenses = 0 
+    savings = 0 
+    debt = 0
     with col1:
-        st.text_input("income: ")
+        income_str = st.text_input("income: ")
+        income = float(income_str) if income_str.strip() else 0
 
     with col2:
-        st.text_input("Expenses: ")
+        expenses_str = st.text_input("Expenses: ")
+        expenses = float(expenses_str) if expenses_str.strip() else 0
 
     with col3:
-        st.text_input("Savings: ")
+        savings_str = st.text_input("Savings: ")
+        savings = float(savings_str) if savings_str.strip() else 0
 
     with col4:
-        st.text_input("Debt: ")
+        debt_str = st.text_input("Debt: ")
+        debt = float(debt_str) if debt_str.strip() else 0
     
-    st.text_input("Financial goals: ")
-    col5, col6 = st.columns(2)
-    with col5:
-        riskTolerance = st.selectbox("Risk tolerance?",
-        ("conservative", "moderate", "aggressive"),
-        )
-    with col6:    
-        timeline = st.selectbox("Risk tolerance?",
-        ("short-term", "medium-term",  "long-term"),
-        )
+    print("Details", income, expenses, savings, debt)
+    submitted = st.form_submit_button(
+        "Get investing amount ?"
+    )
 
-    
-    submitted = st.form_submit_button("Submit")
     if submitted:
+        print("Details=====>", income, expenses, savings, debt)
         with st.spinner("Wait, please. I am working on it..."):
-            result.append(getAdvice())
+            investment = get_investment_amount(income, expenses, savings, debt)
+            if(investment > 0):
+                st.success(f'{investment} is the amount that can be invested', icon="✅")
+            else:
+                st.warning('There is no amount left to invest', icon="⚠️")
+
+    
+st.text_input("Financial goals: ")
+col5, col6 = st.columns(2)
+with col5:
+    riskTolerance = st.selectbox("Risk tolerance?",
+    ("conservative", "moderate", "aggressive"),
+    )
+with col6:    
+    timeline = st.selectbox("Risk tolerance?",
+    ("short-term", "medium-term",  "long-term"),
+    )
 
 
-if(len(result)):
-    st.markdown(result[0]["response"])
+marketTrends = st.button("Get Latest Market Trends")
+if marketTrends:
+    with st.spinner("Wait, please. Fetching latest trends and opportunities..."):
+        text = parseURL()
+        list.append(extractData(text))
+
+# if(len(result)):
+#     st.markdown(result[0]["response"])
+
+if(len(list)):
+    st.markdown(list[0])
