93fb5e65b923e7cbee653202a253b66564c3701f
Mohammad Bilal <mohdbilal16@gmail.com>
2024-11-24 17:40:34 +0530

    fund allocation added

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ environment_config = EnvironmentConfig(
 environment = client.create_environment_endpoint(json_body=environment_config)
 
 st.set_page_config(
-    page_title = "Investment Advisor Agent"
+    page_title = "Lyzr Advyzr"
 )
 
 
@@ -61,7 +61,7 @@ if not openai_api_key:
 
 os.environ["OPENAI_MODEL_NAME"] = 'gpt-3.5-turbo'
 
-st.title("Investment Advisor Agent")
+st.title("Lyzr Advyzr")
 st.write("Personalized Investment Portfolio Advisor using Lyzr's API.")
 open_ai_text_completion_model = OpenAIModel(
     api_key=openai_api_key,
@@ -71,20 +71,6 @@ open_ai_text_completion_model = OpenAIModel(
         "max_tokens":1500
     }
 )
-
-def get_investment_amount(income, expenses, savings, debt):
-    try:
-        available_for_investment = income - expenses- savings - debt
-
-        # Ensure the investment amount is non-negative
-        investment_amount = max(0, available_for_investment)
-
-        return investment_amount
-
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
-
 
 def searchWeb(query):
     url = "https://google.serper.dev/search"
@@ -119,7 +105,6 @@ def searchWeb(query):
 
 def parseURL(url):
     try:
-        print("URL+++",url)
         response = requests.get(url)
         response.raise_for_status()
 
@@ -135,7 +120,9 @@ def researchData(query):
     result = searchWeb(query)
     data = []
     for i in result:
-        if("smallcase" in i.lower() or "mstock" in i.lower() or "screener" in i.lower()):
+        print("URL+++",i)
+        target_domains = {"smallcase", "mstock", "screener", "groww","etmoney"} 
+        if any(domain in i.lower() for domain in target_domains):
             parsed_data = parseURL(i)
             data.append(parsed_data)
     return data[:6]
@@ -160,7 +147,7 @@ def extractData(text):
             model=open_ai_text_completion_model,
             agent=analyst_agent,
             instructions=f"""Analyse the list of companies from {truncated_text} along with their some financial details in tabular format. 
-            Do not write any extra details just the name of the companies is enough."""
+            Do not write any extra details just the name on of the companies is enough."""
         )
 
         output = LinearSyncPipeline(
@@ -175,47 +162,84 @@ def extractData(text):
         print(f"Error URL not working {url}:{e}")
         return None
 
+
+def get_investment_amount(income, expenses, savings, debt):
+    try:
+        available_for_investment = income - expenses- savings - debt
+
+        # Ensure the investment amount is non-negative
+        investment_amount = max(0, available_for_investment)
+
+        return investment_amount
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+
+def fund_allocation(list,riskTolerance,timeline,investment,financial_goal):
+    try:
+        print("FUND",list,riskTolerance,timeline,investment,financial_goal)
+
+        analyst_agent = Agent(
+            role="wealth investor",
+            prompt_persona=f"""Act like an experienced financial advisor with 20 years of expertise in wealth management, retirement planning, and investment strategies. Your clients range from individuals to small business owners seeking guidance on optimizing their financial health. Provide comprehensive, step-by-step advice that takes into account both {riskTolerance} and {timeline} needs."""
+        )
+
+        task1 = Task(
+            name="Wealth investment",
+            model=open_ai_text_completion_model,
+            agent=analyst_agent,
+            instructions=f"""You are an experienced investment advisor specializing in portfolio optimization. 
+            Your task is to distribute a given {investment} amount across these {list} of companies based on the following inputs:
+    {riskTolerance}: Adjust allocations to balance between high-risk and low-risk companies.
+    {timeline}: Optimize the allocation for short-term or long-term growth.
+    {financial_goal}: Prioritize stability, income generation, or aggressive growth as specified.
+    Ensure the distribution is diversified, aligns with financial best practices,in rupees and maximizes the likelihood of achieving the {financial_goal}. Provide clear percentages and amount for allocation to each company and justify your reasoning
+            """
+        )
+
+        output = LinearSyncPipeline(
+            name="Fund Manager",
+            completion_message="Fund Allocated",
+            tasks=[
+                task1
+            ]
+        ).run()
+        return output[0]['task_output']
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+
+
 result=[]
 list = []
 investment = ''
-with st.form("personal_finance"):
-    col1, col2, col3, col4 = st.columns(4)
-    income = 0
-    expenses = 0 
-    savings = 0 
-    debt = 0
-    with col1:
-        income_str = st.text_input("income: ")
-        income = float(income_str) if income_str.strip() else 0
 
-    with col2:
-        expenses_str = st.text_input("Expenses: ")
-        expenses = float(expenses_str) if expenses_str.strip() else 0
+col1, col2, col3, col4 = st.columns(4)
+income = 0
+expenses = 0 
+savings = 0 
+debt = 0
+with col1:
+    income_str = st.text_input("Income: ")
+    income = float(income_str) if income_str.strip() else 0
 
-    with col3:
-        savings_str = st.text_input("Savings: ")
-        savings = float(savings_str) if savings_str.strip() else 0
+with col2:
+    expenses_str = st.text_input("Expenses: ")
+    expenses = float(expenses_str) if expenses_str.strip() else 0
 
-    with col4:
-        debt_str = st.text_input("Debt: ")
-        debt = float(debt_str) if debt_str.strip() else 0
+with col3:
+    savings_str = st.text_input("Savings: ")
+    savings = float(savings_str) if savings_str.strip() else 0
+
+with col4:
+    debt_str = st.text_input("Debt: ")
+    debt = float(debt_str) if debt_str.strip() else 0
     
-    print("Details", income, expenses, savings, debt)
-    submitted = st.form_submit_button(
-        "Get investing amount ?"
-    )
-
-    if submitted:
-        #print("Details=====>", income, expenses, savings, debt)
-        with st.spinner("Wait, please. I am working on it..."):
-            investment = get_investment_amount(income, expenses, savings, debt)
-            if(investment > 0):
-                st.success(f'{investment} is the amount that can be invested', icon="✅")
-            else:
-                st.warning('There is no amount left to invest', icon="⚠️")
-
-    
-st.text_input("Financial goals: ")
+financial_goal =  st.text_input("Financial goal: ")
 col5, col6 = st.columns(2)
 with col5:
     riskTolerance = st.selectbox("Risk tolerance?",
@@ -227,6 +251,7 @@ with col6:
     )
 
 
+
 marketTrends = st.button("Get Latest Market Trends")
 if marketTrends:
     print("D++++",riskTolerance,timeline)
@@ -234,9 +259,12 @@ if marketTrends:
         query = f"What are the best {timeline} stocks with {riskTolerance} risk currently ?"
         text = researchData(query)
         list.append(extractData(text))
-
-# if(len(result)):
-#     st.markdown(result[0]["response"])
-
 if(len(list)):
-    st.markdown(list[0])
+    investment = get_investment_amount(income, expenses, savings, debt)
+    if(investment > 0):
+        st.success(f'{investment} is the amount that can be invested', icon="✅")
+    else:
+        st.warning('There is no amount left to invest', icon="⚠️")
+    allocation = fund_allocation(list,riskTolerance,timeline,investment,financial_goal)
+    if(allocation):
+        st.markdown(allocation)
